2dde441501862553b3de6f2473598c6b2e9f12c8
0xedev <oxdeve@gmail.com>
2025-03-17 10:46:00 +0100

    one-time- approval

--- src/components/PvcSection/components/CreateGameVsComp.tsx
+++ src/components/PvcSection/components/CreateGameVsComp.tsx
@@ -504,9 +504,7 @@ const FlipCoin = () => {
             <div className="bg-white p-10 rounded-lg shadow-lg">
               <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-purple-500"></div>
               <p className="mt-4 text-center">
-                {state.isApproving
-                  ? "Approving tokens (one-time setup)..."
-                  : "Flipping..."}
+                {state.isApproving ? "Approving tokens..." : "Flipping..."}
               </p>
             </div>
           </div>
