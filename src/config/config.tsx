9cf1b551be75c25c5c8e430186ffcb048988e184
0xedev <oxdeve@gmail.com>
2025-03-25 15:31:19 +0100

    error

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,31 +1,24 @@
-import { baseSepolia } from 'wagmi/chains'
+import { base } from "wagmi/chains";
 import { QueryClient } from "@tanstack/react-query";
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { createConfig, http } from "wagmi";
 import { createClient } from "viem";
 
-
-
 export const wagmiConfig = createConfig({
-  chains: [ baseSepolia],
-  connectors: [
-    farcasterFrame()
-   
-  ],
+  chains: [base],
+  connectors: [farcasterFrame()],
   client({ chain }) {
     return createClient({
       chain,
       batch: { multicall: true },
       pollingInterval: 12_000,
-      transport: http("https://base-sepolia.g.alchemy.com/v2/23jsJTJNwRJR6RJBcO6NCcgBuK01TNdX" ), // Corrected this line
+      transport: http(
+        "https://base-mainnet.g.alchemy.com/v2/23jsJTJNwRJR6RJBcO6NCcgBuK01TNdX"
+      ), // Corrected this line
     });
   },
 });
 
 export const queryClient = new QueryClient();
 
-
 export const projectId = import.meta.env.VITE_PROJECT_ID;
-
-
-
